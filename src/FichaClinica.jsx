88357b9a6fa2cd666895262a2b0e18ee1de7661e
tomascarignano <tomas@festinafinance.com>
2024-02-07 21:55:32 +0100

    refactor FichaClinica.jsx

--- src/FichaClinica.jsx
+++ src/FichaClinica.jsx
@@ -1,44 +1,59 @@
 import { useState } from "react";
 
-function FichaClinicaTable({ consultas }) {
-  const rows = [];
-  const [keyEditing, setkeyEditing] = useState("");
+export default function FichaClinica() {
+  return <FichaClinicaTable consultas={CONSULTAS} />;
+}
 
-  consultas.forEach((consulta) => {
-    rows.push(
-      consulta.key === keyEditing ? (
-        <tr>
-          <td>{consulta.fecha}</td>
-          <td>
-            {consulta.texto}
-            <button onClick={() => setkeyEditing("")}>Ok</button>
-          </td>
-        </tr>
-      ) : (
-        <tr>
-          <td>{consulta.fecha}</td>
-          <td>
-            {consulta.texto}
-            {keyEditing === "" && (
-              <button onClick={() => setkeyEditing(consulta.key)}>
-                Editar
-              </button>
-            )}
-          </td>
-        </tr>
-      ),
-    );
-  });
+function FichaClinicaTable({ consultas }) {
+  const [keyEditing, setkeyEditing] = useState("");
 
   return (
     <table>
-      <tbody>{rows}</tbody>
+      <tbody>
+        {consultas.map((consulta) => {
+          return consulta.key === keyEditing ? (
+            <FichaFormulario key={consulta.key} consulta={consulta} onSuccess={() => setkeyEditing("")} />
+          ) : (
+            <tr key={consulta.key}>
+              <td>{consulta.fecha}</td>
+              <td>
+                {consulta.texto}
+                {keyEditing === "" && <button onClick={() => setkeyEditing(consulta.key)}>Editar</button>}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
     </table>
   );
 }
 
+function FichaFormulario({ consulta, onSuccess }) {
+  console.log(Object.entries(consulta.campos));
+
+  return (
+    <tr>
+      <td>{consulta.fecha}</td>
+      <td>
+        {consulta.texto}
+        <button onClick={onSuccess}>Ok</button>
+      </td>
+      {Object.entries(consulta.campos).map(([clave, valor]) => (
+        <td key={clave}>
+          [{clave} - {valor}]
+        </td>
+      ))}
+    </tr>
+  );
+}
+
 const CONSULTAS = [
-  { key: "1", fecha: "05/08/23", texto: "Apple" },
+  {
+    key: "1",
+    fecha: "05/08/23",
+    texto: "Apple",
+    campos: { mc: "dolor de cabeza", diagnostico: "jaqueca", evolucion: "se le paso el dolor" },
+  },
   { key: "2", fecha: "11/10/23", texto: "Dragonfruit" },
   { key: "3", fecha: "15/11/23", texto: "Passionfruit" },
   { key: "4", fecha: "01/12/23", texto: "Spinach" },
@@ -46,6 +61,21 @@ const CONSULTAS = [
   { key: "6", fecha: "01/01/24", texto: "Peas" },
 ];
 
-export default function FichaClinica() {
-  return <FichaClinicaTable consultas={CONSULTAS} />;
-}
+// fecha, clave, valor)
+
+// fecha, (clave, valor), fecha, (calve, valor), fecha
+
+// cunsulta
+// 1, pac1, fecha1
+// 2, pac1, fecha2
+
+// 4, pac2, fecha1
+// 5, pac2, fecha2
+
+// clavevalor
+// consultaid, clave, valor
+// 1, mc, dolor de cabeza
+// 1, diagnostico, jaqueca
+// 2, evolucion, se le paso el dolor de cabeza, lo llamo jpara dentro de un MessageChannel
+
+// consultas -> [{ id: string, fecha: Date, campos: { clave: valor } }]
